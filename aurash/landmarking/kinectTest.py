61864816fe4635ab06d57acb614f37fc4be96d34
aurashn <aurashnorouzi@gmail.com>
2015-10-18 09:13:17 -0500

    minor changes

---
+++ aurash/landmarking/kinectTest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+import cv2
+import sys
+import os
+import cv2.cv as cv
+
+filename = os.getcwd()+'/Data'
+faceCascade = cv2.CascadeClassifier(filename+"/haarcascade_frontalface_default.xml")
+
+video_capture = cv2.VideoCapture(900)
+print video_capture
+
+while True:
+    # If OpenCV cannot grab images
+    r=video_capture.grab()
+    print r
+    if not r:
+        print "Unable to Grab Frames from camera"
+        break
+    
+    # Capture frame-by-frame
+    ret, frame = video_capture.retrieve(1,cv.CV_CAP_OPENNI_GRAY_IMAGE)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
