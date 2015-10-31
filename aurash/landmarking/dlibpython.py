3d0807040c3ac5622d0e324f46b7ed4c6cc682df
aurashn <aurashnorouzi@gmail.com>
2015-10-31 13:06:29 -0500

    Python Opencv code for DLIB

---
+++ aurash/landmarking/dlibpython.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image and
+#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
+#   points on the face such as the corners of the mouth, along the eyebrows, on
+#   the eyes, and so forth.
+#
+#   This face detector is made using the classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
+#   and sliding window detection scheme.  The pose estimator was created by
+#   using dlib's implementation of the paper:
+#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
+#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
+#   and was trained on the iBUG 300-W face landmark dataset.
+#
+#   Also, note that you can train your own models using dlib's machine learning
+#   tools. See train_shape_predictor.py to see an example.
+#
+#   You can get the shape_predictor_68_face_landmarks.dat file from:
+#   http://sourceforge.net/projects/dclib/files/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2
+#
+# COMPILING THE DLIB PYTHON INTERFACE
+#   Dlib comes with a compiled python interface for python 2.7 on MS Windows. If
+#   you are using another python version or operating system then you need to
+#   compile the dlib python interface before you can use this file.  To do this,
+#   run compile_dlib_python_module.bat.  This should work on any operating
+#   system so long as you have CMake and boost-python installed.
+#   On Ubuntu, this can be done easily by running the command:
+#       sudo apt-get install libboost-python-dev cmake
+#
+#   Also note that this example requires scikit-image which can be installed
+#   via the command:
+#       pip install -U scikit-image
+#   Or downloaded from http://scikit-image.org/download.html. 
+
+import sys
+import dlib
+import cv2
+import numpy as np
+import Image
+
+
+
+predictor_path = sys.argv[1]
+
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+win = dlib.image_window()
+
+video_capture = cv2.VideoCapture(0)
+
+
+while True:
+    # Capture frame-by-frame
+    ret, img = video_capture.read()
+    list=[]
+    b,g,r = cv2.split(img)
+    img = cv2.merge([r,g,b])
+     #Ask the detector to find the bounding boxes of each face. The 1 in the
+     #second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    dets = detector(img, 1)
+    print("Number of faces detected: {}".format(len(dets)))
+    for i in dets:
+        shape = predictor(img, i)
+        for a in xrange(68):
+            b=shape.part(a)
+            list.append((b.x,b.y))
+
+    #Draw the face landmarks on the screen.
+  
+        win.clear_overlay()
+        win.add_overlay(shape)
+        win.set_image(img)
+        win.add_overlay(dets)
+    
