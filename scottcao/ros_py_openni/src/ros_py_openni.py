cacb495b925c01fdc0708cb4546b670633087833
aurashn <aurashnorouzi@gmail.com>
2015-11-01 12:19:08 -0600

    new changes

--- scottcao/ros_py_openni/src/ros_py_openni.py
+++ scottcao/ros_py_openni/src/ros_py_openni.py
@@ -1,99 +1,121 @@
 #!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
+# coding: utf-8 
+
+# In[ ]: 
 
 import openni as opi
+
 import numpy as np
+
 from random import randrange
+
 import sys
+
 import rospy
+
 from rospy.numpy_msg import numpy_msg
-from rospy_tutorials.msg import Floats
+
+from rospy_tutorials.msg import Floats 
 
 
 def publisher(): 
 
     rospy.init_node('OpenNI', anonymous=True)
+
     rgb_pub = rospy.Publisher('rgb', numpy_msg(Floats), queue_size=10)
-    depth_pub = rospy.Publisher('depth', numpy_msg(Floats), queue_size=10)
-    ir_pub = rospy.Publisher('ir', numpy_msg(Floats), queue_size=10)
-    rate = rospy.Rate(10) # 10hz
+
+    depth_pub = rospy.Publisher('depth', numpy_msg(Floats), queue_size=10) 
+
+    rate = rospy.Rate(10) # 10hz 
 
     # #### Create context and generators
+
     ctx = opi.Context()
-    ctx.init()
+
+    ctx.init() 
 
     depth_generator = opi.DepthGenerator()
+
     depth_generator.create(ctx)
+
     depth_generator.set_resolution_preset(opi.RES_VGA)
-    depth_generator.fps = 30
+
+    depth_generator.fps = 30 
 
     image_generator = opi.ImageGenerator()
-    image_generator.create(ctx)
 
-    ir_generator = opi.IRGenerator()
-    ir_generator.create(ctx)
-    ir_generator.set_resolution_preset(opi.RES_VGA)
-    ir_generator.fps = 30
+    image_generator.create(ctx) 
 
     gesture_generator = opi.GestureGenerator()
+
     gesture_generator.create(ctx)
-    gesture_generator.add_gesture('Wave')
+
+    gesture_generator.add_gesture('Wave') 
 
 
     # #### Write callbalks ...
-    def gesture_detected(src, gesture, id, end_point):
-        pass
 
-    def gesture_progress(src, gesture, point, progress): 
-        print "Emma Watson is waving !!", src
+    def gesture_detected(src, gesture, id, end_point):
+
+        pass 
+
+    def gesture_progress(src, gesture, point, progress):
+
+        print "Emma Watson is waving !!", src 
 
 
     # #### Register callbacks ...
+
     gesture_generator.register_gesture_cb(gesture_detected, gesture_progress)
 
 
     # #### Converting and publishing captured data
+
     def capture_rgb():
+
         rgb_frame = np.fromstring(image_generator.get_raw_image_map_bgr(),
+
                               dtype=np.uint8).reshape(480, 640, 3)
+
         # print((type)(rgb_frame))
-        rgb_pub.publish(rgb_frame)
+
+        rgb_pub.publish(rgb_frame) 
 
 
     def capture_depth():
+
         depth_map = np.asarray(depth_generator.get_tuple_depth_map()).reshape((480, 640))
+
         #print((type)(depth_map))
-        depth_pub.publish(depth_map)
+
+        depth_pub.publish(depth_map) 
 
 
-    def capture_IR():
-        ir_map = np.asarray(ir_generator.get_tuple_ir_map()).reshape((480, 640))
-        ir_pub.publish(ir_map)
-
-
-    ctx.start_generating_all()
+    ctx.start_generating_all() 
 
 
     # #### Main loop
+
     while not rospy.is_shutdown():
+
         capture_rgb()
+
         capture_depth()
-        capture_IR()
+
         ctx.wait_any_update_all()
-        rate.sleep()
+
+        rate.sleep() 
 
 
-    # #### Then we stop and close the context
+    # #### Then we stop and close the context 
 
-    # In[ ]:
+    # In[ ]: 
 
     ctx.stop_generating_all()
-    ctx.shutdown()
+
+    ctx.shutdown() 
 
 if __name__ == '__main__':
-    try:
-        publisher()
-    except rospy.ROSInterruptException:
-        pass
+
+    publisher() 
