5a460cdefe363ca73843f6ebd510851f02cf7597
Larry Li <larrylisky@gmail.com>
2016-07-02 11:38:15 -0500

    Added mutex for better control of variables. Preparing for command override.

--- ros_rp2w/src/sys_rp2w.cpp
+++ ros_rp2w/src/sys_rp2w.cpp
@@ -13,6 +13,11 @@
 using namespace std;
 using namespace LibSerial;
 
+const double LINEAR_CONVERSION_FACTOR = 840;
+const double ANGULAR_CONVERSION_FACTOR = 840;
+const double LINEAR_CONVERSION = 1/M_PI*LINEAR_CONVERSION_FACTOR/5*12;
+const double ANGULAR_CONVERSION = ANGULAR_CONVERSION_FACTOR/5*12/360;
+
 rp2w::Status rc;
 boost::shared_ptr<rp2w> robot(new rp2w());
 char digital1 = 0;
@@ -20,11 +25,33 @@ char digital2 = 0x0c;
 int theta_;
 double distance_;
 boost::mutex msg_mutex;
+bool stopped_early;
+double stopping_distance = 1*LINEAR_CONVERSION;
 
-const double LINEAR_CONVERSION_FACTOR = 840;
-const double ANGULAR_CONVERSION_FACTOR = 840;
-const double LINEAR_CONVERSION = 1/M_PI*LINEAR_CONVERSION_FACTOR/5*12;
-const double ANGULAR_CONVERSION = ANGULAR_CONVERSION_FACTOR/5*12/360;
+void initialize() {
+  robot->setGPIO1(digital1);
+  robot->setGPIO2(digital2);
+  robot->setLeftMotorSpeed(0);
+  robot->setRightMotorSpeed(0);
+  robot->setCameraPan(0);
+  robot->setCameraTilt(0);
+}
+
+void update() {
+  rc = robot->update();
+  int fail_count = 0;
+  while (rc != rp2w::OK) {
+    fail_count++;
+    cout << "robot.update failed (" << rc << ") x " << fail_count << endl;
+    robot = boost::shared_ptr<rp2w>(new rp2w());
+    rc = robot->connect("/dev/ttyUSB0");
+    rc = robot->update();
+  }
+  if (fail_count > 0) {
+    cout << "robot reconnected" << endl;
+    initialize();
+  }
+}
 
 void setMotorSpeeds(int turn_speed, int trav_speed) {
   digital1 = robot->getGPIO1();
@@ -49,24 +76,7 @@ void setMotorSpeeds(int turn_speed, int trav_speed) {
   robot->setGPIO1(digital1);
   robot->setLeftMotorSpeed(left_motor);
   robot->setRightMotorSpeed(right_motor);
-  rc = robot->update();
-  int fail_count = 0;
-  while (rc != rp2w::OK) {
-    fail_count++;
-    cout << "robot.update failed (" << rc << ") x " << fail_count << endl;
-    robot = boost::shared_ptr<rp2w>(new rp2w());
-    rc = robot->connect("/dev/ttyUSB0");
-    rc = robot->update();
-  }
-  if (fail_count > 0) {
-    cout << "robot reconnected" << endl;
-    digital1 = 0;
-    digital2 = 0;
-    robot->setGPIO1(digital1);
-    robot->setGPIO2(digital2);
-    robot->setLeftMotorSpeed(0);
-    robot->setRightMotorSpeed(0);
-  }
+  update();
   // cout << "Motor speeds set to TURN SPEED " << turn_speed 
   //      << " And TRAVEL SPEED " << trav_speed << endl;
 }
@@ -78,52 +88,7 @@ void command(const ros_rp2w::AdvancedCommand::ConstPtr& msg) {
   distance_ = msg->distance;
 }
 
-int main(int argc, char **argv) {
- ros::init(argc, argv, "rp2w");
- ros::NodeHandle n;
-
- rc = robot->connect("/dev/ttyUSB0");
- rc = robot->update();
- if (rc != rp2w::OK) {
-   ROS_ERROR("No RP2W robot.");
-   return 1;
- }
- else {
-   cout << "RP2W Initialized. " << endl;
- }
-
- robot->setGPIO1(digital1);
- robot->setGPIO2(digital2);
- robot->setLeftMotorSpeed(0);
- robot->setRightMotorSpeed(0);
- robot->setCameraPan(0);
- robot->setCameraTilt(0);
- 
- ros::Publisher pub = n.advertise<ros_rp2w::Packet>("rp2w_packet", 10);
- ros::Subscriber sub = n.subscribe("rp2w/advanced_command", 1, command);
- ros::Rate loop_rate(60);
- bool stopped_early;
- double stopping_distance = 1*LINEAR_CONVERSION;
-
- while (ros::ok()) {
-  rc = robot->update();
-  int fail_count = 0;
-  while (rc != rp2w::OK) {
-    fail_count++;
-    cout << "robot.update failed (" << rc << ") x " << fail_count << endl;
-    robot = boost::shared_ptr<rp2w>(new rp2w());
-    rc = robot->connect("/dev/ttyUSB0");
-    rc = robot->update();
-  }
-  if (fail_count > 0) {
-    cout << "robot reconnected" << endl;
-    digital1 = 0;
-    digital2 = 0;
-    robot->setGPIO1(digital1);
-    robot->setGPIO2(digital2);
-    robot->setLeftMotorSpeed(0);
-    robot->setRightMotorSpeed(0);
-  }
+void move(ros::Rate loop_rate) {
   if (msg_mutex.try_lock()) {
     int theta = theta_;
     int theta_conv = abs(theta*ANGULAR_CONVERSION);
@@ -221,17 +186,16 @@ int main(int argc, char **argv) {
       // cout << "End: " << now << endl;
     }
   }
-  ros_rp2w::Packet packet;
+}
 
+ros_rp2w::Packet createPacket() {
+  ros_rp2w::Packet packet;
   packet.leftMotorSpeed = (uint8_t)(robot->getLeftMotorSpeed());
   packet.rightMotorSpeed = (uint8_t)(robot->getRightMotorSpeed());
   packet.cameraTilt = robot->getCameraTilt();
   packet.cameraPan = robot->getCameraPan();
-  // packet.digital1 = (uint8_t)(robot.getGPIO1());
-  // packet.digital2 = (uint8_t)(robot.getGPIO2());
   packet.digital1 = (uint8_t)(digital1);
   packet.digital2 = (uint8_t)(digital2);
-
   packet.encoderA = (uint32_t)(robot->getEncoderA());
   // cout << packet.encoderA << endl;
   packet.encoderB = (uint32_t)(robot->getEncoderB());
@@ -244,14 +208,36 @@ int main(int argc, char **argv) {
   // ROS_INFO("rear sonar: %u", (unsigned char)(packet.rearSonar));
   packet.bumper = (uint8_t)(robot->getBumper());
   // ROS_INFO("bumper: %u", (unsigned char)(packet.bumper));
+  return packet;
+}
 
-  pub.publish(packet);
+int main(int argc, char **argv) {
+ ros::init(argc, argv, "rp2w");
+ ros::NodeHandle n;
+ ros::Rate loop_rate(60);
 
+ rc = robot->connect("/dev/ttyUSB0");
+ rc = robot->update();
+ if (rc != rp2w::OK) {
+   ROS_ERROR("No RP2W robot.");
+   return 1;
+ }
+ else {
+   cout << "RP2W Initialized. " << endl;
+ }
+ 
+ ros::Publisher pub = n.advertise<ros_rp2w::Packet>("rp2w_packet", 10);
+ ros::Subscriber sub = n.subscribe("rp2w/advanced_command", 1, command);
+
+ initialize();
+
+ while (ros::ok()) {
+  update();
+  move(loop_rate);
+  pub.publish(createPacket());
   ros::spinOnce();
   loop_rate.sleep();
 }
 
 return 0;
 }
-
-
