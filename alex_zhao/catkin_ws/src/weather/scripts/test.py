764266e85e5822d6d0e9213f30dabca26b1c7f01
Syngerical <azhao6@gmail.com>
2017-09-10 14:23:00 -0500

    Delete test.py

--- alex_zhao/catkin_ws/src/weather/scripts/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import ast
-
-def test(city):
-    dictionary = {'fruit': 'apple', 'color': 'red', 'plant': 'rose'}
-    print dictionary[1]
-if __name__ == "__main__":
-    test('SugarLand')
